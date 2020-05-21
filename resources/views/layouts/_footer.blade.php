d0c93f1888b602db7bcbdc186bede3fd61cd6eee
Your Name <you@example.com>
2020-05-21 11:53:58 +0000

    基础页面结构

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+  <div class="container">
+    <p class="float-left">
+      由 <a href="https://cong-sun.herokuapp.com/" target="_blank">Summer</a> WeiboApp <span style="color: #e27575;font-size: 14px;">❤</span>
+    </p>
+
+    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+  </div>
+</footer>
