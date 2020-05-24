084306da450b215f5c9a07b5694c61cc034506e8
Your Name <you@example.com>
2020-05-24 12:35:38 +0000

    填充用户和话题数据

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    public $timestamps = false
+    public $timestamps = false;
 
     protected $fillable = [
     	'name','description',
